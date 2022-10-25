a3734eb7f6be3000706d9539183b88d483318c3e
devMunyi <samunyi90@gmail.com>
2022-10-25 16:15:46 +0300

    imported dependencies

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+/* eslint-disable */
+// import bootstrap css minified version
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import bootstrap js minified version
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+// import jquery to use bs js components
+import $ from 'jquery';
+
+// import jquery to use bs js components
+import Popper from 'popper.js';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -8,7 +21,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
