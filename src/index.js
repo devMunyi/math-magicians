086200f4324274f0469c9192ca5a8e824af58418
devMunyi <samunyi90@gmail.com>
2022-11-01 16:01:08 +0300

    update routers and nomatch page

--- src/index.js
+++ src/index.js
@@ -28,9 +28,9 @@ root.render(
     <Router>
       <NavBar />
       <Routes>
-        <Route index path="/" element={<HomePage />} />
-        <Route path="/calculator" element={<CalculatorPage />} />
-        <Route path="/qoute" element={<QuotePage />} />
+        <Route index path="/math-magicians" element={<HomePage />} />
+        <Route path="/math-magicians/calculator" element={<CalculatorPage />} />
+        <Route path="/math-magicians/qoute" element={<QuotePage />} />
         <Route path="*" element={<NoMatch />} />
       </Routes>
     </Router>
--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
 const NoMatch = () => (
-  <div
-    className="display-1 d-flex justify-content-center align-item-center"
-    style={{ height: '100%' }}
-  >
-    <h1>Not Found</h1>
+  <div className="nomatch">
+    <h1>Page Not Found!</h1>
   </div>
 );
 export default NoMatch;
