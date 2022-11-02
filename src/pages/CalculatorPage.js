e93417102ec56b6858b04e5f37526ce781e871db
devMunyi <samunyi90@gmail.com>
2022-11-02 20:05:27 +0300

    install and use react-icons library

--- src/pages/CalculatorPage.js
+++ src/pages/CalculatorPage.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import React from 'react';
 import Calculator from '../components/Calculator';
-import Quote from './Quote';
+import { FaHandPointRight } from 'react-icons/fa';
 
 const CalculatorPage = () => {
   const quote = "Let's do some maths!";
@@ -10,7 +10,9 @@ const CalculatorPage = () => {
     <div className="container mt-5">
       <div className="row">
         <div className="col-md-5">
-          <h4>{quote}</h4>
+          <h4>
+            {quote} <FaHandPointRight />
+          </h4>
         </div>
         <div className="col-md-7">
           <Calculator />
